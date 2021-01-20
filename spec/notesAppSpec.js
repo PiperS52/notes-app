99d18b62f85c948c4acdba3c235223064865c5bc
PiperS52 <simon.a.piper87@gmail.com>
2021-01-20 20:30:29 +0000

    refactored and added new tests

--- spec/notesAppSpec.js
+++ spec/notesAppSpec.js
@@ -1,50 +1,68 @@
+'use strict;'
+
 function newNote() {
-  let description = "if NoteList is an empty array on construction"
-  let note = new Note(); 
-  expect(note.NoteList).toEqual(0);
+  let note = new Note();
+  it("NoteList is an empty array on construction", function(){
+    expect(note.NoteList.length).toEqual(0);
+  })
 };
 
-
 function noteListCheck() {
-  let description = "it returns the full note list"
   let note = new Note();
-  note.createNote("Helloooooo")
-  note.createNote("k bye")
-  var x = note.getFullNoteList();
-  expect(note.getFullNoteList().length).toEqual(2)
-}
+  it("getFullNoteList returns the full content of NoteList", function(){
+    note.createNote("Helloooooo");
+    note.createNote("k bye");
+    expect(note.getFullNoteList().length).toEqual(2);
+  })
+};
 
 function checkCreateNote() {
-  let description = "it checks the createNote function"
   let note = new Note();
-  note.createNote("hello")
-  expect(note.NoteList[0]).toChangeBy(1)
-}
+  it("it checks the createNote function", function(){
+    note.createNote("hello");
+    expect(note.NoteList.length).toEqual(1);
+  })
+
+  let noteOne = new Note();
+  let noteTwo = new Note();
+  it("it checks that createNote adds a new entry to NoteList", function(){
+    noteOne.createNote("hello");
+    expect(noteOne.NoteList.length).toBeGreaterThan(noteTwo.NoteList.length);
+  })
+};
 
 function checkAbbrevNoteList() {
-  let description = "it checks the Abbrev note array is working"
   let note = new Note();
-  note.createNote("hello, I am a super long note, like way longer than 20c")
-  note.createNote("Whoah so am I, so weird, I'm also like a way long note")
-  expect(note.getAbbrevNoteList()[0].length, 20, description)
-}
+  it("it checks the Abbrev note array is working", function(){
+    note.createNote("hello, I am a super long note, like way longer than 20c");
+    expect(note.getAbbrevNoteList()[0].length).toEqual(20);
+  })
+};
 
-try {
-  newNote();
-}
-catch (error) {
-  console.log("error")
-}
-try { 
-  noteListCheck();
-}
-catch (error) {
+// try {
+//   newNote();
+// }
+// catch (error) {
+//   console.log("error")
+// }
+// try {
+//   noteListCheck();
+// }
+// catch (error) {
+//
+// }
+// try {
+//   checkCreateNote();
+// }
+// catch (error) {
+//
+// }
+// checkAbbrevNoteList();
 
-}
-try { 
-  checkCreateNote();
-}
-catch (error) {
 
-}
-  checkAbbrevNoteList();
+//
+newNote();
+noteListCheck();
+checkCreateNote();
+checkCreateNote();
+checkAbbrevNoteList();
